804e083cf6642700a2f395c3e2a91a03773d5cf6
Tyler Abbassi <tfa21@scarletmail.rutgers.edu>
2025-12-10 00:46:04 -0500

    added api router to CORS

--- main.py
+++ main.py
@@ -49,7 +49,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
